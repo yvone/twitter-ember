1892406386887c0bb3950b6f98449cc7f99e554a
Enovy <yvone.sre@gmail.com>
2019-03-31 22:53:13 -0600

    Feature: listing current tweets, WIP: empty state for Feed component

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,10 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
 	model () {
-		return [
-			{ id: 1, user_name: "Yvone", description: "hola, hola holaaa", created_at: "2019-04-01" },
-			{ id: 2, user_name: "Yvone", description: "segundo", created_at: "2019-04-01" },
-			{ id: 3, user_name: "Yvone", description: "tercero", created_at: "2019-04-01" }
-		];
+		return this.store.peekAll('tweet');
 	}
 }
