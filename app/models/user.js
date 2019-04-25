fb91f6ed4eda2a2878334b7617ae3234e029eaaa
Enovy <yvone.sre@gmail.com>
2019-04-24 23:47:15 -0500

    Feature: (playing with tracked properties in model) generate custom avatars with Identicon

--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,6 @@
+import Identicon from 'identicon.js';
+import sha1 from 'js-sha1';
+
 import DS from 'ember-data';
 const { Model, attr, hasMany } = DS;
 
@@ -6,4 +9,10 @@ export default class UserModel extends Model {
 	@attr('string') username;
 	@attr('string') bio;
 	@hasMany('tweet') tweets;
+
+	get avatar(){
+		let hash = sha1(this.username);
+		let data = new Identicon(hash).toString();
+		return `data:image/png;base64,${data}`;
+	};
 }
